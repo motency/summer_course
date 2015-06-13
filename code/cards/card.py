209cdc56dae50df26bad1aecf79ff65b351d8ec4
Annie <motency@gmail.com>
2015-06-12 19:31:05 -0700

    Update card.py

--- code/cards/card.py
+++ code/cards/card.py
@@ -1,11 +1,12 @@
 # card.py
-# -------
-# by ______
+# ===*===
+# by Annie Leonard
 
 # Give a description of this module here...
 
 class Card:
     "A card represents a playing card"
+    from random import random
 
     suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
@@ -14,5 +15,7 @@ class Card:
 
     def __init__(self, card_number):
         pass
+    def __repr__(self):
+        return "This card is a {} of {}".format(self.ranks, self.suits)
 
 
