35996c85d9a5a6e565fa98aa28d973d24fe552f9
Annie <motency@gmail.com>
2015-06-12 21:30:10 -0700

    Update card.py

--- code/cards/card.py
+++ code/cards/card.py
@@ -1,21 +1,18 @@
 # card.py
 # ===*===
 # by Annie Leonard
-
-# Give a description of this module here...
+# This module should create a card with a randomly generated suit and rank.
 
 class Card:
     "A card represents a playing card"
-    from random import random
+    import random
 
     suits = ["Clubs", "Diamonds", "Hearts", "Spades"]
     ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-    short_suits = ["C", "D", "H", "S"]
-    short_ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
 
-    def __init__(self, card_number):
-        pass
+    def __init__(self, card_rank, card_suit):
+        self.card_rank = random.choice(ranks)
+        self.card_suit = random.choice(suits)
     def __repr__(self):
-        return "This card is a {} of {}".format(self.ranks, self.suits)
-
-
+        return "This card is a {} of {}".format(self.card_rank, self.card_suits)
+playing_card = Card(card_rank, card_suit)
